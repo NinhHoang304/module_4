f3ea5523b687155965708c7e6539261206c16ee3
HoangJerry304 <hoangninh3004@gmail.com>
2023-02-06 16:16:26 +0700

    ss5

---
+++ ss5_orm/exercise/product_manager_orm/src/main/java/com/example/repository/IProductRepository.java
@@ -0,0 +1,17 @@
+package com.example.repository;
+
+import com.example.model.Product;
+
+import java.util.List;
+
+public interface IProductRepository {
+    List<Product> findAll();
+
+    Product findById(int id);
+
+    void save (Product product);
+
+    void update (Product product);
+
+    void remove(int id);
+}
